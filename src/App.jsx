0b8505c6e826e73999949b93d0e4547d1eb84888
MikeSDC <michael.sundelacruz@gmail.com>
2025-11-09 14:04:24 -0500

    Lesson 54 - added useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "./components/Header/Header.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
@@ -5,8 +6,12 @@ import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("Please click a button.");
+
   function handleSelect(selectedButton) {
     // selectedButton => 'components, 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
     console.log(`Hello World! - selected ${selectedButton}!`);
   }
 
@@ -26,12 +31,15 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
             <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
           Dynamic Content
+          <div>{selectedTopic}</div>
         </section>
       </main>
     </div>
