9ed320e4c8b1db53cb67af8955da3568b9db48ce
MikeSDC <michael.sundelacruz@gmail.com>
2025-11-10 20:40:17 -0500

    Lesson 57 - completed dynamic styling

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -2,11 +2,11 @@
 // import { reactDescriptions } from "../../data.js";
 import "./TabButton.css";
 
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
 
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>{children}</button>
     </li>
   );
 }
