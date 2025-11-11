2bd49b7956f8b23a189b7bdf442e00d658a366d2
MikeSDC <michael.sundelacruz@gmail.com>
2025-11-10 20:16:57 -0500

    Lesson 56 - completed rendering content conditionally lesson, used if statement rather than ternary.

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,30 @@ import Header from "./components/Header/Header.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 
-import { CORE_CONCEPTS } from "./data";
+import { EXAMPLES, CORE_CONCEPTS } from "./data";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button.");
+  const [selectedTopic, setSelectedTopic] = useState("");
 
   function handleSelect(selectedButton) {
     // selectedButton => 'components, 'jsx', 'props', 'state'
     setSelectedTopic(selectedButton);
     console.log(selectedTopic);
     console.log(`Hello World! - selected ${selectedButton}!`);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -38,8 +52,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          Dynamic Content
-          <div>{selectedTopic}</div>
+          {tabContent}
         </section>
       </main>
     </div>
