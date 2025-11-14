3381c41d73477efe6dfaeac2cb549bb4de5de912
MikeSDC <michael.sundelacruz@gmail.com>
2025-11-13 19:50:39 -0500

    Lesson 62 - introduce fragments

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -79,7 +79,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
