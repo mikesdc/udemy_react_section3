5df220f46917ba5873182e0a54712e13780aeb5d
MikeSDC <michael.sundelacruz@gmail.com>
2025-11-15 10:25:50 -0500

    Lesson 68 - setting component types dynamically

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -3,10 +3,11 @@
 // import "./TabButton.css";
 
 export default function TabButton({ children, isSelected, ...props }) {
-
   return (
     <li>
-      <button className={isSelected ? "active" : undefined} {...props}>{children}</button>
+      <button className={isSelected ? "active" : undefined} {...props}>
+        {children}
+      </button>
     </li>
   );
 }
